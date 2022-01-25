4c29d78be72c02b92709de2cb92ccce2f41bd0a5
gamuniz <gmuniz@redhat.com>
2022-01-25 16:29:22 -0500

    Update custom.py

--- custom.py
+++ custom.py
@@ -6,6 +6,6 @@ import requests
 def main():
   res=requests.get('https://raw.githubusercontent.com/gamuniz/Inventory_Test/master/custom.json')
   data=res.json()
-  print json.dumps(data, sort_keys=True, indent=2)
+  print(json.dumps(data, sort_keys=True, indent=2))
 if __name__ == '__main__':
   main()
